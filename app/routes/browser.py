7e889098e2d919690d0043ff699f9453b9354b98
yosider <yosider.on.earth@gmail.com>
2024-02-02 16:25:19 +0900

    Refactor: separate environment runner

--- app/routes/browser.py
+++ app/routes/browser.py
@@ -1,14 +1,9 @@
-import asyncio
-from typing import List
-
-import gym
-import numpy as np
-import robohive.envs.arms  # noqa: F401 # type: ignore
 from aiortc import VideoStreamTrack
 from av import VideoFrame
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 from app.app_state import AppState
+from app.env import EnvRunner
 from app.utils.webrtc import createPeerConnection, handle_answer, handle_candidate, handle_offer_request
 
 router = APIRouter()
@@ -66,62 +61,6 @@ async def ws_browser(websocket: WebSocket):
         if pc:
             await pc.close()
             print("/browser: Peer connection closed")
-
-
-class EnvRunner:
-    def __init__(self, state: AppState):
-        self.num_agents = state.num_agents
-
-        self.env = gym.make("FrankaReachFixedMulti-v0")
-        self.a_dim_per_agent = self.env.action_space.shape[0] // self.num_agents
-
-        self.frames: List[np.ndarray | None] = [None] * self.num_agents
-        self.frame_update_cond = asyncio.Condition()
-        self.command = state.command  # updated globally
-
-    def start(self):
-        self.task = asyncio.create_task(self._run())
-        # TODO: separate thread?
-
-    async def stop(self):
-        self.task.cancel()
-        try:
-            await self.task
-        except asyncio.CancelledError:
-            pass
-
-    async def _run(self):
-        print("env_process started")
-        env = self.env
-
-        # init
-        obs = env.reset()
-
-        while True:
-            action = self._get_action(obs, self.command)
-            obs, _, done, _ = env.step(action)
-            visuals = env.get_visuals()
-
-            async with self.frame_update_cond:
-                for i in range(self.num_agents):
-                    self.frames[i] = visuals[f"rgb:franka{i}_front_cam:256x256:1d"].reshape((256, 256, 3))
-                self.frame_update_cond.notify_all()
-
-            if done:
-                env.reset()
-
-            await asyncio.sleep(0.03)
-
-    def _get_action(self, obs, command):
-        # TODO: use policy
-
-        action = self.env.action_space.sample()
-        # zero actions for agents with command 0
-        for i in range(self.num_agents):
-            if command[i] == 0:
-                action[i * self.a_dim_per_agent : (i + 1) * self.a_dim_per_agent] = 0
-
-        return action
 
 
 class ImageStreamTrack(VideoStreamTrack):
