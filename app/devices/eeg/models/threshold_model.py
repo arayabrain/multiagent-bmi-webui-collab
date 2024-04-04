e2dd17c96ebb6a378d1dc94a316efc5a0f14bf85
yosider <yosider.on.earth@gmail.com>
2024-04-04 21:06:09 +0900

    modify to output a command only once per threshold exceedance

--- app/devices/eeg/models/threshold_model.py
+++ app/devices/eeg/models/threshold_model.py
@@ -9,10 +9,14 @@ class ThresholdModel:
         self.thres = thres
         self.baseline_rms = baselines["rms"]
 
+        self.class_output_flags = [False] * num_classes
+        self.flag_reset_margin = 0.2
+
     def __call__(self, data: np.ndarray) -> tuple[int | None, np.ndarray]:
         rms = root_mean_square(data)
         rms_ratio = rms / self.baseline_rms
 
+        # ensure the length of rms_ratio is equal to the number of classes
         if len(rms_ratio) < self.num_classes:
             # zero-padding at the end
             rms_ratio = np.pad(rms_ratio, (0, self.num_classes - len(rms_ratio)))
@@ -20,13 +24,20 @@ class ThresholdModel:
             # truncate
             rms_ratio = rms_ratio[: self.num_classes]
 
-        max_ch = int(np.argmax(rms_ratio))
-        if rms_ratio[max_ch] > self.thres:
-            cls = max_ch  # output the channel index as class
-        else:
-            cls = None  # no output class
-
         likelihoods = rms_ratio / self.thres
+        max_cls = int(np.argmax(likelihoods))
+        if likelihoods[max_cls] < 1 or self.class_output_flags[max_cls]:
+            # if the max_cls is already output, do not output the same class again
+            cls = None
+        else:
+            cls = max_cls
+            self.class_output_flags[max_cls] = True
+
+        # reset the output flag if the likelihood is below (1 - margin)
+        for i in range(self.num_classes):
+            if likelihoods[i] < 1 - self.flag_reset_margin:
+                self.class_output_flags[i] = False
+
         return cls, likelihoods
 
 
@@ -35,13 +46,17 @@ class ThresholdDiffModel:
         self.num_classes = num_classes
         self.thres = thres
         self.baseline_rms = baselines["rms"]
+
         self.prev_rms = baselines["rms"]
+        self.class_output_flags = [False] * num_classes
+        self.flag_reset_margin = 0.2
 
     def __call__(self, data: np.ndarray) -> tuple[int | None, np.ndarray]:
         rms = root_mean_square(data)
         rms_ratio_diff = (rms - self.prev_rms) / self.baseline_rms
         self.prev_rms = rms
 
+        # ensure the length of rms_ratio_diff is equal to the number of classes
         if len(rms_ratio_diff) < self.num_classes:
             # zero-padding at the end
             rms_ratio_diff = np.pad(rms_ratio_diff, (0, self.num_classes - len(rms_ratio_diff)))
@@ -49,11 +64,18 @@ class ThresholdDiffModel:
             # truncate
             rms_ratio_diff = rms_ratio_diff[: self.num_classes]
 
-        max_ch = int(np.argmax(rms_ratio_diff))
-        if rms_ratio_diff[max_ch] > self.thres:
-            cls = max_ch  # output the channel index as class
-        else:
-            cls = None  # no output class
-
         likelihoods = np.maximum(rms_ratio_diff, 0) / self.thres  # ignore negative values
+        max_cls = int(np.argmax(likelihoods))
+        if likelihoods[max_cls] < 1 or self.class_output_flags[max_cls]:
+            # if the max_cls is already output, do not output the same class again
+            cls = None
+        else:
+            cls = max_cls
+            self.class_output_flags[max_cls] = True
+
+        # reset the output flag if the likelihood is below (1 - margin)
+        for i in range(self.num_classes):
+            if likelihoods[i] < 1 - self.flag_reset_margin:
+                self.class_output_flags[i] = False
+
         return cls, likelihoods
