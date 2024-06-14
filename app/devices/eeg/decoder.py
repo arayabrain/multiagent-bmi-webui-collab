436aa99f9e8d1debf2e506b6cdac4367cfe04543
dosssman <dosssman@hotmail.fr>
2024-06-14 19:48:56 +0900

    Removed EEG related code; not relevant to 16 agents envs

--- app/devices/eeg/decoder.py
+++ app/devices/eeg/decoder.py
@@ -9,6 +9,7 @@ from typing import Union, Tuple
 from app.devices.utils.networking import extract_buffer
 from app.devices.utils.utils import array2str
 
+
 class Decoder:
     """Decode the EEG signal using a model."""
 
@@ -70,49 +71,3 @@ class Decoder:
             self.subscription.dispose()
         self.is_running = False
         print("Decoder stopped.")
-
-def measure_baseline(
-    input_observable: rx.Observable,
-    baseline_duration: float,
-    baseline_ready_duration: float,
-    input_freq: int,
-    auto_start: bool = False,
-) -> dict[str, np.ndarray | float]:
-    """Measure the baseline of the signal from the input observable."""
-
-    baselines: dict[str, np.ndarray | float] = {"average": 0, "rms": 1}
-    baseline_ready = threading.Event()
-
-    def set_baseline(data: np.ndarray):
-        baselines["average"] = np.mean(data, axis=0)  # (channels,)
-        baselines["rms"] = root_mean_square(data)  # (channels,)
-        print(f"Average: {array2str(baselines['average'])}")
-        print(f"Root mean square: {array2str(baselines['rms'])}")
-        baseline_ready.set()
-
-    # prompt user to keep still
-    confirm = auto_start or click.confirm(
-        f"\nPreparing to measure the baseline. Press Enter, then relax and stay still."
-        f"\nMeasurement will start in {baseline_ready_duration}s and will continue for {baseline_duration}s.",
-        default=True,
-    )
-    if not confirm:
-        print("Baseline measurement cancelled. Using average=0, rms=1 as default.")
-        return baselines
-
-    print(f"Starting baseline measurement in {baseline_ready_duration}s...")
-    time.sleep(baseline_ready_duration)
-
-    print("Measuring baseline...")
-    baseline_subscription = input_observable.pipe(  # type: ignore
-        ops.buffer_with_count(int(baseline_duration * input_freq)),
-        ops.take(1),  # take only the first buffer
-        ops.map(lambda buf: extract_buffer(buf)[0]),  # type: ignore  # (time, channels)
-    ).subscribe(
-        on_next=set_baseline,
-        on_completed=lambda: print("Baseline measurement completed.\n"),
-    )
-    baseline_ready.wait()
-    baseline_subscription.dispose()
-
-    return baselines
