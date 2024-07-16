dbba693b3598a240df78b29c59574d9e3f3d03d2
keggily <hannah.kodama.douglas@gmail.com>
2024-07-16 14:04:02 +0900

    fixed file path and anonymize other users in user specific file

--- app/utils/anonymize.py
+++ app/utils/anonymize.py
@@ -4,24 +4,19 @@ import hashlib
 import json
 import sys
 
+
 def hash_string(username):
     return hashlib.sha256(username.encode()).hexdigest()
-
 def anonymize_session(expid):
     session_path = os.path.join(os.pardir, 'logs', expid)
     print(session_path)
-
-
     history_file = os.path.join(session_path, 'history.jsonl')
     info_file = os.path.join(session_path, 'info.json')
-
     # Dictionary to store original to anonymized username mappings
     username_mapping = {}
-
     # Read and process the history file
     with open(history_file, 'r') as f:
         history = f.readlines()
-
     new_lines = []
     for line in history:
         match = re.search(r'"username": "(.*?)"', line)
@@ -32,15 +27,12 @@ def anonymize_session(expid):
             anonymized_username = username_mapping[username]
             line = re.sub(r'"username": "(.*?)"', f'"username": "{anonymized_username}"', line)
         new_lines.append(line)
-
     # Write the new lines back to the history file
     with open(os.path.join(session_path, 'anon-history.jsonl'), 'w') as f:
         f.writelines(new_lines)
-
     # Read and process the info file
     with open(info_file, 'r') as f:
         info_data = json.load(f)
-
     # Anonymize usernames in the info file
     if 'usernames' in info_data:
         anonymized_usernames = []
@@ -49,37 +41,29 @@ def anonymize_session(expid):
                 username_mapping[username] = hash_string(username)
             anonymized_usernames.append(username_mapping[username])
         info_data['usernames'] = anonymized_usernames
-
     # Write the updated info back to the info file
     with open(os.path.join(session_path, 'anon-info.json'), 'w') as f:
         json.dump(info_data, f, indent=4)
-
-
     session_users = username_mapping.keys()
     for username in session_users:
         # If user path is anonymized, keep same. If user path still username, rename to anonymized username
         anonymized_username = username_mapping[username]
-        user_path = os.path.join(os.pardir, 'logs', anonymized_username, expid)
+        user_path = os.path.join(os.pardir, 'logs', anonymized_username)
         if not os.path.exists(user_path):
             #rename directory
-            os.rename(os.path.join(os.pardir, 'logs', username, expid), user_path)
-
-        with open(os.path.join(user_path, 'info.json'), 'r') as f:
+            os.rename(os.path.join(os.pardir, 'logs', username), user_path)
+        with open(os.path.join(user_path, expid, 'info.json'), 'r') as f:
             user_info_data = json.load(f)
         #replace instance of username with anonymized username
         if user_info_data['name'] == username:
             user_info_data['name'] = anonymized_username
-            user_info_data['user_list'] = [anonymized_username if user == username else user for user in user_info_data['user_list']]
-        with open(os.path.join(user_path, 'anon-info.json'), 'w') as f:
+            user_info_data['user_list'] = [hash_string(user) for user in user_info_data['user_list']]
+        with open(os.path.join(user_path, expid, 'anon-info.json'), 'w') as f:
             json.dump(user_info_data, f, indent=4)
-
-
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python anonymize_session.py <expid1> <expid2> ...")
         sys.exit(1)
-    
     expids = sys.argv[1:]
     for expid in expids:
         anonymize_session(expid)
